8a906ea35f1f816302c1fe32337188a5f4152336
Nnamdi <nnamdiibns@gmail.com>
2016-12-17 01:15:31 +0100

    Added a a few basic tests

--- studentable/tests.py
+++ studentable/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
+from studentable.models import Course
+
 # Create your tests here.
+class StudentableTest(TestCase):
+	def test_default_course_creation(self):
+		code = 'abc123'
+		self.course = Course.objects.create(code=code)
+		self.assertEqual(self.course.mon, ' ')
+		self.assertEqual(self.course.tue, ' ')
+		self.assertEqual(self.course.wed, ' ')
+		self.assertEqual(self.course.thu, ' ')
+		self.assertEqual(self.course.fri, ' ')
+
+	def test_nondefault_course_creation(self):
+		code = 'abc123'
+		time = '12pm-14pm'
+		self.course = Course.objects.create(code=code, mon=time, fri=time)
+
+		self.assertEqual(self.course.mon, time)
+		self.assertEqual(self.course.tue, ' ')
+		self.assertEqual(self.course.wed, ' ')
+		self.assertEqual(self.course.thu, ' ')
+		self.assertEqual(self.course.fri, time)
+
